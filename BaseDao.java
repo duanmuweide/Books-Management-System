9ba6b46c0cb7db5f1499317f29ac625b405f9ced
Chzm-code <yinxiamengjiang@qq.com>
2024-12-03 09:38:52 +0800

    Add files via upload

---
+++ BaseDao.java
@@ -0,0 +1,21 @@
+package com.book_system.dao;
+
+import java.util.List;
+
+public class BaseDao {
+	
+	// 查询单条记录（管理员查询借书还书记录）
+	Object[] getOne(String sql, Object... params);
+	
+	// 查询多条记录（管理员查询借书还书记录）
+	List<Object[]> getMany(String sql, Object... params);
+	
+	//执行插入（包含用户注册的插入，管理员添加书的插入）
+	int executeInsert(String sql, Object... params);
+	
+	//执行管理员对图书的删除(返回受影响的行数)
+	int executeDelete(String sql, Object... params);
+	
+	//执行管理员对图书的更新或用户对个人信息的修改
+	int executeUpdate(String sql, Object... params);
+}
