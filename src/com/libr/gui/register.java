63ddac3aba806fb0a8564188950170381a51e429
nihao151 <2443743398@qq.com>
2024-12-16 16:28:33 +0800

    Update register.java

--- src/com/libr/gui/register.java
+++ src/com/libr/gui/register.java
@@ -1,7 +1,21 @@
 package com.libr.gui;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class register extends JFrame {
     public register() {
@@ -49,6 +63,48 @@ public class register extends JFrame {
 
         // 设置窗口居中
         setLocationRelativeTo(null);
+        
+        submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//获取注册的内容
+				String id = idField.getText();
+				String name=nameField.getText();
+				String realname=realNameField.getText();
+				String address=addressField.getText();
+				String email=emailField.getText();
+				String major=majorField.getText();
+				String selectedgender = (String) genderComboBox.getSelectedItem();
+				// getPassword()返回的是一个char[]
+				char[] chs = passwordField.getPassword();
+				// 这里将一个char[]转成一个字符串
+				String pwd = new String(chs);
+				
+				//正则表达式判断信息是否合适
+				if(!Pattern.matches("^[1-9]\\d{9}$",id)) {
+					JOptionPane.showMessageDialog(register.this, "id应有十位数字且不能以0开头");
+				}
+				else if(!Pattern.matches("([\\u4E00-\\u9FA5]|[a-zA-Z]|[0-9])+",name)) {
+					JOptionPane.showMessageDialog(register.this, "名称只能是汉字字母数字，且不能为空");
+				}else if(!Pattern.matches("^(?=\\S*[0-9])(?=\\S*[A-Za-z])\\S{8,}$",pwd)) {
+					JOptionPane.showMessageDialog(register.this, "密码至少八位且要包含数字和字母");
+				}else if(!Pattern.matches("([\\u4E00-\\u9FA5]|[a-zA-Z])+",realname)) {
+					JOptionPane.showMessageDialog(register.this, "真名只能是汉字字母，且不能为空");
+				}else if(!Pattern.matches("([\\u4E00-\\u9FA5]|[0-9])+",address)) {
+					JOptionPane.showMessageDialog(register.this, "地址只能是汉字数字，且不能为空");
+				}else 
+					if(!Pattern.matches("\\\\w+@\\\\w+(\\\\.\\\\w+)+",email)) {
+					JOptionPane.showMessageDialog(register.this, "邮箱格式不对");
+				}else if(!Pattern.matches("[\\u4E00-\\u9FA5]+",major)) {
+					JOptionPane.showMessageDialog(register.this, "专业只能为且不能为空");
+				}else  {
+					//添加到数据库中
+					
+				}
+				
+				
+			}
+		});
+        
     }
 
     private void addLabelAndField(String labelText, JTextField field, GridBagConstraints gbc, int y) {
