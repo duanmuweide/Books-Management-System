50a6755538174dec92e3e618fa0037f9ec03daab
nihao151 <2443743398@qq.com>
2024-12-16 16:25:45 +0800

    Update deletebook.java

--- src/com/libr/gui/deletebook.java
+++ src/com/libr/gui/deletebook.java
@@ -4,6 +4,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.regex.Pattern;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,23 +13,23 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class deletebook extends JFrame {
+	  // 创建输入组件
+    JTextField idField = new JTextField("请输入ID",20);
+    JTextField nameField = new JTextField(20);
+    JTextField authorField = new JTextField(20);
+    JTextField typeField = new JTextField(20);
+    JTextField statusField = new JTextField(20);
+    JTextField locationField = new JTextField(20);
+    JTextField numberField = new JTextField(20);
     public deletebook() {
-        setTitle("修改个人信息");
-        setSize(400, 300);
+        setTitle("删除图书");
+        setSize(400, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(5, 5, 5, 5);
 
-        // 创建输入组件
-        JTextField idField = new JTextField("请输入ID",20);
-        JTextField nameField = new JTextField(20);
-        JTextField authorField = new JTextField(20);
-        JTextField typeField = new JTextField(20);
-        JTextField statusField = new JTextField(20);
-        JTextField locationField = new JTextField(20);
-        JTextField numberField = new JTextField(20);
         // 添加组件到界面
         int y = 0;
         
@@ -43,10 +44,15 @@ public class deletebook extends JFrame {
         // 添加删除按钮
         JButton deleteButton = new JButton("删除");
         gbc.gridx = 1;
-        gbc.gridy = y;
+        gbc.gridy = y++;
         gbc.gridwidth = 2;
         gbc.anchor = GridBagConstraints.CENTER; // 设置按钮居中
         add(deleteButton, gbc);
+        
+      //添加返回按钮
+        JButton backButton = new JButton("返回");
+        gbc.gridy = y;
+        add(backButton, gbc);
 
         // 设置窗口居中
         setLocationRelativeTo(null);
@@ -55,7 +61,23 @@ public class deletebook extends JFrame {
         //删除按钮监听事件
         deleteButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				 String id=idField.getText();
+	    	     String name=nameField.getText();
+	    	     String type=typeField.getText();
+	    	     String author=authorField.getText();
+	    	     String location=locationField.getText();
+	    	     String number=numberField.getText();
+	    	     String status=statusField.getText();
+	    	     //执行删除操作
+			}
+		});
+        
+        backButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				deletebook.this.dispose();
+				manager_system frame = new manager_system();
+				frame.setVisible(true);
+
 			}
 		});
 
@@ -82,7 +104,19 @@ public class deletebook extends JFrame {
   
         editButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	
+            	 String id=idField.getText();
+	    	     String name=nameField.getText();
+	    	     String type=typeField.getText();
+	    	     String author=authorField.getText();
+	    	     String location=locationField.getText();
+	    	     String number=numberField.getText();
+				//正则表达式判断信息是否合适
+				if(!Pattern.matches("[0-9]+",id)) {
+					JOptionPane.showMessageDialog(deletebook.this, "id不能为空");
+				}else  {
+					//查看图书信息操作
+					
+				}
             }
         });
     }
